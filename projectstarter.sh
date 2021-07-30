236c841d73a63fdca19714e1bd200ab6089add65
Ryan Carlson <ryan.carlson@mercsystems.ca>
2021-07-30 11:52:28 -0600

    finished

---
+++ projectstarter.sh
@@ -0,0 +1,20 @@
+#!usr/bin/bash
+mkdir w12c;
+cd w12c;
+touch index.html;
+echo "creating new project";
+echo "hello world" index.html;
+mkdir css;
+cd css;
+touch style.css;
+cd ..;
+mkdir js;
+cd js;
+touch app.js;
+cd w12c;
+echo "All done!";
+exit;
+
+
+
+
